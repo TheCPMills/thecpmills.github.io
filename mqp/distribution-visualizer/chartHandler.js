89d8191da7031eaf91dd3a29609a96f9f5b922fa
Chase Miller <cpmills26@gmail.com>
2023-11-20 14:56:04 -0500

    Added colors to bars

--- mqp/distribution-visualizer/chartHandler.js
+++ mqp/distribution-visualizer/chartHandler.js
@@ -1,9 +1,9 @@
-var nBox, mBox, chart;
+var nBox, mBox, chartArea, chart;
 
 function setup() {
     nBox = document.getElementById("n");
     mBox = document.getElementById("m");
-    chart = document.getElementById('chart');
+    chartArea = document.getElementById('chart');
 
     nBox.value = "1";
     mBox.value = "1";
@@ -12,50 +12,123 @@ function setup() {
 }
 
 function setupDefaultCanvas() {
-    // make chart fill the screen minus the header
-    chart.width = window.innerWidth;
-    chart.height = window.innerHeight - 150;
+    // make chartArea fill the screen minus the header
+    chartArea.width = window.innerWidth;
+    chartArea.height = window.innerHeight - 150;
 
 
     // put text that says "Enter values for n and m to generate a distribution"
-    var ctx = chart.getContext('2d');
+    var ctx = chartArea.getContext('2d');
     ctx.font = "24px Arial";
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
-    ctx.fillText("Enter values for n and m to generate a distribution", chart.width / 2, chart.height / 2);
+    ctx.fillText("Enter values for n and m to generate a distribution", chartArea.width / 2, chartArea.height / 2);
 }
 
-function generateDistribution() {
+async function generateDistribution() {
+    // clear the canvas
+    if (chart != null) {
+        chart.destroy();
+    }
+
     var n = parseInt(nBox.value);
     var m = parseInt(mBox.value);
 
     var strings = [];
     var occurrences = [];
-    $.getJSON("https://thecpmills.com/mqp/res/files/" + n + "x" + m + ".json", function (data) {
-        strings = data.stringOccurences.map(function (e) {
-            return e[0];
-        });
-        occurrences = data.stringOccurences.map(function (e) {
-            return e[1];
-        });
-    });
 
-    new Chart(chart, {
+    if (n > m) {
+        var fileName = m + "x" + n + ".json";
+    } else {
+        var fileName = n + "x" + m + ".json";
+    }
+
+    var gradientMap = generateGradient([0xfde724, 0x79d151, 0x29788e, 0x404387, 0x440154], Math.min(n,m) + 1);
+    var barColors = [];
+    for (var i = 0; i <= Math.min(n,m); i++) {
+        var color = "#" + gradientMap[i].toString(16);
+        for (var j = 0; j < Math.pow(2, i); j++) {
+            barColors.push(color);
+        }
+    }
+
+    console.log(barColors);
+
+    const jsonResponse = await fetch("https://thecpmills.com/mqp/res/files/" + fileName);
+    var json = await jsonResponse.text();
+    var data = JSON.parse(json);
+    var stringOccurrences = data.stringOccurences;
+
+    for (var key in stringOccurrences) {
+        strings.push("\"" + stringOccurrences[key][0] + "\"");
+        occurrences.push(stringOccurrences[key][1]);
+    }
+
+    chart = new Chart(chartArea, {
         type: 'bar',
         data: {
             labels: strings,
             datasets: [{
-                label: '# of Occurrences',
+                label: 'Number of Occurrences',
                 data: occurrences,
+                backgroundColor: barColors,
                 borderWidth: 1
             }]
         },
         options: {
             scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Longest Common Subsequence'
+                    }
+                },
                 y: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Number of Occurrences'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'Distribution of Longest Common Subsequences of Strings of Length ' + n + ' and ' + m
                 }
             }
         }
     });
+}
+
+function generateGradient(stops, steps) {
+    var colors = [];
+
+    for (var i = 0; i < steps; i++) {
+        colors[i] = getPercent(stops, i / (steps - 1));
+    }
+
+    return colors;
+}
+
+function interpolate(start, end, percent) {
+    var red = (start >> 16) * (1 - percent) + (end >> 16) * percent;
+    var green = ((start >> 8) & 0xff) * (1 - percent) + ((end >> 8) & 0xff) * percent;
+    var blue = (start & 0xff) * (1 - percent) + (end & 0xff) * percent;
+    var color = (red << 16) + (green << 8) + blue;
+    return Math.floor(color);
+}
+
+function getPercent(stops, percent) {
+    var step = 1.0 / (stops.length - 1);
+    for (var i = 0; i < stops.length - 1; i++) {
+        // if percent in between stops i and i + 1
+        if (percent >= step * i && percent <= step * (i + 1)) {
+            return interpolate(stops[i], stops[i + 1], (percent - step * i) / step);
+        }
+    }
+    return -1;
 }
